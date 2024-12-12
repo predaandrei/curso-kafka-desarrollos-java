df74b5f3adf76a0ea5e8b6dd4c6b588b63b9c421
Andrei Preda <predaandrei3@gmail.com>
2024-12-12 18:30:32 +0100

    Update Stock.java

--- core-producer/src/main/java/com/imagina/core_producer/model/Stock.java
+++ core-producer/src/main/java/com/imagina/core_producer/model/Stock.java
@@ -1,13 +1,7 @@
 package com.imagina.core_producer.model;
 
 public class Stock {
-<<<<<<< HEAD
-    private  String symbol;
-    private  String companyName;
-    private double price;
-    private long timestamp;
 
-=======
     private String symbol;
     private String companyName;
     private double price;
@@ -15,8 +9,6 @@ public class Stock {
 
     public Stock() {
     }
-
->>>>>>> a09dc005118e511cf2c2358a8575f9f9500c7bae
     public Stock(String symbol, String companyName, double price, long timestamp) {
         this.symbol = symbol;
         this.companyName = companyName;
@@ -24,56 +16,25 @@ public class Stock {
         this.timestamp = timestamp;
     }
 
-<<<<<<< HEAD
-    public void setSymbol(String symbol) {
-        this.symbol = symbol;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public void setPrice(double price) {
-        this.price = Math.round(price * 100.0)/100.0;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
-
-=======
->>>>>>> a09dc005118e511cf2c2358a8575f9f9500c7bae
     public String getSymbol() {
         return symbol;
     }
-
-<<<<<<< HEAD
-=======
+    
     public void setSymbol(String symbol) {
         this.symbol = symbol;
     }
-
->>>>>>> a09dc005118e511cf2c2358a8575f9f9500c7bae
+    
     public String getCompanyName() {
         return companyName;
     }
-
-<<<<<<< HEAD
-=======
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
 
->>>>>>> a09dc005118e511cf2c2358a8575f9f9500c7bae
     public double getPrice() {
         return price;
     }
 
-<<<<<<< HEAD
-    public long getTimestamp() {
-        return timestamp;
-    }
-=======
     public void setPrice(double price) {
         this.price = Math.round(price * 100.0) / 100.0;
     }
@@ -85,5 +46,4 @@ public class Stock {
     public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
     }
->>>>>>> a09dc005118e511cf2c2358a8575f9f9500c7bae
 }
