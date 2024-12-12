4424cea9a8b5069adc331ed20adfc8bc4604f246
Andrei Preda <predaandrei3@gmail.com>
2024-12-12 18:27:46 +0100

    Update JsonConfig.java

--- core-producer/src/main/java/com/imagina/core_producer/config/JsonConfig.java
+++ core-producer/src/main/java/com/imagina/core_producer/config/JsonConfig.java
@@ -1,30 +1,16 @@
 package com.imagina.core_producer.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-<<<<<<< HEAD
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-=======
 import com.fasterxml.jackson.databind.SerializationFeature;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
->>>>>>> a09dc005118e511cf2c2358a8575f9f9500c7bae
 
 @Configuration
 public class JsonConfig {
 
     @Bean
-<<<<<<< HEAD
     public ObjectMapper objectMapper(){
         ObjectMapper objectMapper = new ObjectMapper();
-=======
-    public ObjectMapper objectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        //objectMapper.registerModule(new JavaTimeModule());
-        //objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
->>>>>>> a09dc005118e511cf2c2358a8575f9f9500c7bae
         return objectMapper;
     }
 }
