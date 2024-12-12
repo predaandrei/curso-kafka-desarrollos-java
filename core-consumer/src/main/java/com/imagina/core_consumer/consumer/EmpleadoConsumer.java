d3c9521e3a710d573c3e451315c4135b4d11f6f9
Andrei Preda <predaandrei3@gmail.com>
2024-12-12 18:33:56 +0100

    Update EmpleadoConsumer.java

--- core-consumer/src/main/java/com/imagina/core_consumer/consumer/EmpleadoConsumer.java
+++ core-consumer/src/main/java/com/imagina/core_consumer/consumer/EmpleadoConsumer.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-<<<<<<< HEAD
 @Service
 @Slf4j
 public class EmpleadoConsumer {
@@ -20,18 +19,5 @@ public class EmpleadoConsumer {
     public void listenEmpleado(String mensaje) throws JsonProcessingException {
         var empleado = objectMapper.readValue(mensaje, Empleado.class);
         log.info("Empleado es {} ", empleado.toString());
-=======
-//@Service
-@Slf4j
-public class EmpleadoConsumer {
-
-    //@Autowired
-    private ObjectMapper objectMapper;
-
-    @KafkaListener(topics = "t-empleado")
-    public void listenEmpleado(String message) throws JsonProcessingException {
-        var empleado = objectMapper.readValue(message, Empleado.class);
-        log.info("El empleado es : {}", empleado.toString());
->>>>>>> a09dc005118e511cf2c2358a8575f9f9500c7bae
     }
 }
