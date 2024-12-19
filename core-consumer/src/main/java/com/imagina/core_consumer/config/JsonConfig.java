485dce10ef50cfcdbb166c5220fc7f0eb40e817b
dmartinezsepulveda.external@epo.org <dmartinezsepulveda.external@epo.org>
2024-12-19 17:19:34 +0100

    fixing Java Time Module import

--- core-consumer/src/main/java/com/imagina/core_consumer/config/JsonConfig.java
+++ core-consumer/src/main/java/com/imagina/core_consumer/config/JsonConfig.java
@@ -1,9 +1,10 @@
 package com.imagina.core_consumer.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class JsonConfig {
@@ -11,8 +12,8 @@ public class JsonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        //objectMapper.registerModule(new JavaTimeModule());
-        //objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }
--- core-producer/src/main/java/com/imagina/core_producer/config/JsonConfig.java
+++ core-producer/src/main/java/com/imagina/core_producer/config/JsonConfig.java
@@ -2,9 +2,9 @@ package com.imagina.core_producer.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 @Configuration
 public class JsonConfig {
@@ -12,8 +12,8 @@ public class JsonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        //objectMapper.registerModule(new JavaTimeModule());
-        //objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return objectMapper;
     }
 }
